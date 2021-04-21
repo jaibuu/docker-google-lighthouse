cbd0fd7ef77668609f0438f56b6dca2bc4681b18
Jairo Narvaez <jairo.narvaez@from.digital>
2021-04-20 20:10:28 -0500

    continue on errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ wget "$@" -O /home/chrome/urls
 while IFS= read -r line || [ -n "$line" ]; do
 
     if [ "${line#http}" != "$line" ]; then
-        echo "Peforming test for $line URL - "; lighthouse --enable-error-reporting --chrome-flags="${CHROME_FLAGS}" "$line" --output json --output html
+        echo "Peforming test for $line URL - "; lighthouse --enable-error-reporting --chrome-flags="${CHROME_FLAGS}" "$line" --output json --output html || true
     fi
 
 
